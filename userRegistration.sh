f678fff3471cd2f5c4c15784cedb44d462e2504d
priyagund <priyagund18@gmail.com>
2019-11-21 18:23:18 +0530

    password have atleast one numric letter

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,14 +2,16 @@
 echo "take input from user"
 read userInput
 
-patternName="^([A-Z][a-zA-Z]{3,}$)"
-patternLastName="^([A-Z][a-zA-Z]{3,}$)"
-patternEmail="^[a-zA-Z0-9]([._+]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2}){0,1}$"
-patternMob="^[0-9]{2}\s[0-9]{10}"
-	if [[ $userInput =~ $patternMob ]]
+patternName1="^([A-Z][a-zA-Z]{3,}$)"
+patternLastName2="^([A-Z][a-zA-Z]{3,}$)"
+patternEmail3="^[a-zA-Z0-9]([._+]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2}){0,1}$"
+patternMob4="^[0-9]{2}\s[0-9]{10}"
+patternPassword5="^[a-zA-Z0-9]{8,}$"
+patternPassword6="^(?=.*?[a-zA-Z])(?=.{8,})$"
+pattrenPassword7="^[a-zA-Z0-9]{8,}(?=.*?[0-9])$"
+	if [[ $userInput =~ $patternPassword2 ]]
 	then
 	echo "valid"
 	else
 	echo "invalid"
 	fi
-
