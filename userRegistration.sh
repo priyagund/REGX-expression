75960a06c366f16735af386a795bffc111506145
priyagund <priyagund18@gmail.com>
2019-11-21 12:43:46 +0530

    ADD:user email checked

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,7 +4,8 @@ read userInput
 
 patternName="^([A-Z][a-zA-Z]{3,}$)"
 patternLastName="^([A-Z][a-zA-Z]{3,}$)"
-	if [[ $userInput =~ $patternLastName ]]
+patternEmail="^[a-zA-Z0-9]([._+]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2}){0,1}$"
+	if [[ $userInput =~ $patternEmail ]]
 	then
 	echo "valid"
 	else
