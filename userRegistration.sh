5ff0d96ba6995845fd5b61c2922486bde5362800
priyagund <priyagund18@gmail.com>
2019-11-21 13:06:36 +0530

    ADD:user password checked atleast 8 character

--- userRegistration.sh
+++ userRegistration.sh
@@ -6,7 +6,8 @@ patternName="^([A-Z][a-zA-Z]{3,}$)"
 patternLastName="^([A-Z][a-zA-Z]{3,}$)"
 patternEmail="^[a-zA-Z0-9]([._+]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2}){0,1}$"
 patternMob="^[0-9]{2}\s[0-9]{10}"
-	if [[ $userInput =~ $patternMob ]]
+patternPassword="^[a-zA-Z]{8,}$"
+	if [[ $userInput =~ $patternPassword ]]
 	then
 	echo "valid"
 	else
